bc589fc968f6ab4dfe5f72aaa9a2435046e822f6
Lugaid85 <124282593+Lugaid85@users.noreply.github.com>
2024-05-13 11:56:00 +0200

    add BottleOfWine, FuelCanister and ExciseProduct

---
+++ src/main/java/pl/edu/agh/mwo/invoice/product/ExciseProduct.java
@@ -0,0 +1,32 @@
+package pl.edu.agh.mwo.invoice.product;
+
+import java.math.BigDecimal;
+
+public class ExciseProduct extends Product {
+    BigDecimal exciseTax;
+    BigDecimal fixedPromotion;
+
+    public ExciseProduct(String name, BigDecimal price, BigDecimal excise) {
+        super(name, price, new BigDecimal("0.23"));
+        exciseTax = excise;
+        fixedPromotion = new BigDecimal("0");
+    }
+
+    @Override
+    public BigDecimal getPriceWithTax() {
+        return super.getPriceWithTax().add(exciseTax).subtract(fixedPromotion);
+    }
+
+    public BigDecimal getExciseTaxValue() {
+        return exciseTax;
+    }
+
+    public BigDecimal getFixedPromotion() {
+        return fixedPromotion;
+    }
+
+    public void setFixedPromotion(BigDecimal promotion){
+        fixedPromotion = promotion;
+    }
+
+}
---
+++ src/main/java/pl/edu/agh/mwo/invoice/product/FuelCanister.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.mwo.invoice.product;
+
+import java.math.BigDecimal;
+
+public class FuelCanister extends ExciseProduct {
+    public FuelCanister(String name, BigDecimal price, boolean isTaxFreeDay) {
+        super(name, price, new BigDecimal("5.56"));
+        if (isTaxFreeDay)
+            super.setFixedPromotion(super.getExciseTaxValue());
+    }
+}
